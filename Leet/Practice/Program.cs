67d5b92ac9d196a4b86a732e80163dccfcbb0758
squall.sung <squall.sung@wanin.tw>
2023-10-02 16:32:05 +0800

    修改 run test2

--- Leet/Practice/Program.cs
+++ Leet/Practice/Program.cs
@@ -12,10 +12,14 @@ namespace Practice
 			int[][] array = new int[2][];
 			array[0] = new int[2] { 3, 3};
             array[1] = new int[2] { 3, 3};
-            //array[2] = new int[3] { 2, 7, 7 };
+			//array[2] = new int[3] { 2, 7, 7 };
 
+			List<List<string>> source = new List<List<string>>();
+			source.Add(new List<string> { "a", "a", "a", "b", "a" });
+			source.Add(new List<string> { "a", "b", "a", "b", "a" });
+			source.Add(new List<string> { "a", "a", "a", "b", "c" });
 
-            var ans = solution.MaxVowels("weallloveyou", 7);
+			var ans = solution.Test2(source);
 
 			Console.WriteLine($"ans:{ans}");
 		}
